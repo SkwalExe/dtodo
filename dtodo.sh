a29527912496272fd9fc2a23f8f0d2f176d4662e
SkwalExe <support@skwal.net>
2022-03-22 05:54:33 +0100

    added comments

--- dtodo.sh
+++ dtodo.sh
@@ -20,43 +20,44 @@ reset='\033[0m'
 
 
 
-function printTodo() {
-    content=$(cat -b $1)
+function printTodoList() { # Prints the specified (first argument passed to the function) todo list
+    content=$(cat -b $1) # -b to add lien number
 
-    if [[ -z ${content// } ]]; then
+    if [[ -z ${content// } ]]; then # the // is to remove trailing spaces
         printf "${blue}[ i ] : The daily todo list is empty \n"
         exit 1;
     else
         printf "\n${blue}$content\n"
-
     fi
-
 }
 
-function checkExist() {
-    if [ ! -f ~/.$tdfile ]; then
-        printf "${red}[ x ] : ~/.$tdfile not found.\n"
-        printf "${blue}[ i ] : Creating ~/.${tdfile}...\n"
-        if touch ~/.$tdfile; then
-            printf "${green}[ v ] : .$tdfile created successfully\n"
-        else
+function checkExist() { # Check if the current todo file ($tdfile) exists
+    if [ ! -f ~/.$tdfile ]; then #
+        printf "${blue}[ i ] : Creating ~/.${tdfile}...\n" 
+        if ! touch ~/.$tdfile; then # try to create the file, else print error and exit
             printf "${red}[ x ] : Failed to create ~/.$tdfile\n"
             exit 1;
         fi
     fi
 }
-tdfile="dtodo"
 
-function parseArgs() {
+tdfile="dtodo"  # the todo file to execute the action on 
+                # the todo files are ~/.dtodo-*
+                # the default todo file is ~/.dtodo
 
 
-    command=${1:-default}
+
+function parseArgs() { # parse command line arguments
+
+
+    command=${1:-default} # the command to execute on the todo list # if not argument is specified, default command (printall) is used
+   
     case "$command" in
 
-        l|list)
+        l|list) # specify which todo list to execute the command on
             if [ $# -gt 1 ]; then
                     shift
-                    _tdfile="$1"
+                    _tdfile="$1" # temporary variable to store the todo list
                 
                     if [[ -z ${_tdfile// } ]]; then
                         printf "${red}[ x ] : Missing argument for list\n"
@@ -64,7 +65,7 @@ function parseArgs() {
                     else
                         tdfile="dtodo-$_tdfile"
                         shift
-                        parseArgs $@
+                        parseArgs $@ # parse args again to execute the specified command on the just specified todo list
                     fi
                 
                 else
@@ -74,12 +75,12 @@ function parseArgs() {
 
         ;;
 
-        rl|removelist)
+        rl|removelist) # delete a todo list
             if [ $# -gt 1 ]; then
                     shift
-                    toremove="$1"
+                    toremove="$1" # variable to store the todo list to remove
                 
-                    if [[ -z ${toremove// } ]]; then
+                    if [[ -z ${toremove// } ]]; then 
                         printf "${red}[ x ] : Missing argument for removelist\n"
                         exit 1;
                     else
@@ -94,17 +95,17 @@ function parseArgs() {
         ;;
 
 
-        p|print)
+        p|print) # print the current todo list
             checkExist
-            [ $# -gt 1 ] && printf "${yellow}[ /i\\ ] : print doesn't require any arguments \n"
-            printTodo ~/.$tdfile
+            [ $# -gt 1 ] && printf "${yellow}[ /i\\ ] : print doesn't require any arguments \n" # if there is more than one argument print a warning because the arguments are ignored
+            printTodoList ~/.$tdfile
         ;;
 
-        a|add)
+        a|add) # add a new task to the current todo list
             checkExist
             if [ $# -gt 1 ]; then
                 shift
-                taskName="$*"
+                taskName="$*" # variable to store the task name - equal to all the remaining arguments after the command
             
                 if [[ -z ${taskName// } ]]; then
                     printf "${red}[ x ] : Missing argument for add\n"
@@ -112,7 +113,7 @@ function parseArgs() {
                 else
                     printf "\r${blue}[ i ] : Adding task: $taskName\n"
                     printf "[ ] - $taskName\n" >> ~/.$tdfile
-                    printTodo ~/.$tdfile
+                    printTodoList ~/.$tdfile
                 fi
             
             else
@@ -121,49 +122,49 @@ function parseArgs() {
             fi
         ;;
 
-        c|clear)
-            checkExist
-            [ $# -gt 1 ] && printf "${yellow}[ /i\\ ] : clear doesn't require any arguments \n"
+        c|clear) # remove all tasks from the current todo list
+            checkExist 
+            [ $# -gt 1 ] && printf "${yellow}[ /i\\ ] : clear doesn't require any arguments \n" # if there is more than one argument print a warning because the arguments are ignored
             printf "\r${blue}[ i ] : Clearing the daily todo list\n"
-            printf "" > ~/.$tdfile
-            printTodo  ~/.$tdfile
+            printf "" > ~/.$tdfile # overwrite the todo list
+            printTodoList  ~/.$tdfile
         ;;
 
-        d|done)
-            checkExist
+        d|done) # mark a task as done
+            checkExist 
             shift
-            taskId="${1:-first}"    
+            taskId="${1:-first}" # the task id to mark as done "first" if no argument is specified
 
             content=$(cat ~/.$tdfile)
             if [[ -z ${content// } ]]; then
                 printf "\r${blue}[ i ] : The daily todo list is empty \n"
                 exit 1;
             else
-                if [[ $taskId == 'first' ]]; then
+                if [[ $taskId == 'first' ]]; then # if no argument is specified mark the first undone task as done
                     printf "\r${blue}[ i ] : Marking first undone task as done\n"
-                    sed -i '0,/\[ \]/{s/\[ \]/\[x\]/}' ~/.$tdfile
+                    sed -i '0,/\[ \]/{s/\[ \]/\[x\]/}' ~/.$tdfile # on the first line containing [ ] replace it with [x]
                 else
-                    re='^[0-9]+$'
+                    re='^[0-9]+$' # regex to check if the argument is a number
 
-                    while [ $# -gt 0 ]; do
+                    while [ $# -gt 0 ]; do # while there are arguments
                         taskId="$1"
 
-                        [[ ! $taskId =~ $re ]] && printf "${red}[ x ] : Invalid task id\n" && exit 1;
+                        [[ ! $taskId =~ $re ]] && printf "${red}[ x ] : Invalid task id\n" && exit 1; # if the argument is not a number print error and exit
                         
                         printf "${blue}[ i ] : Marking task $taskId as done\n"
-                        sed -i "${taskId}s/\[ \]/\[x\]/" ~/.$tdfile
-                        shift
+                        sed -i "${taskId}s/\[ \]/\[x\]/" ~/.$tdfile # on the specified line replace [ ] with [x]
+                        shift # remove the task id from the arguments list and parse the remaining arguments
                     done
                 fi
-                printTodo ~/.$tdfile
+                printTodoList ~/.$tdfile
             fi
         
         ;;
 
-        u|undo)
+        u|undo) # mark a task as undone
             checkExist
-            shift  
-            taskId="${1:-first}"    
+            shift 
+            taskId="${1:-first}" # the task id to mark as undone "first" if no argument is specified
 
 
             content=$(cat ~/.$tdfile)
@@ -173,27 +174,27 @@ function parseArgs() {
             else
                 if [[ $taskId == 'first' ]]; then
                     printf "\r${blue}[ i ] : Marking first done task as undone\n"
-                    sed -i '0,/\[x\]/{s/\[x\]/\[ \]/}' ~/.$tdfile
+                    sed -i '0,/\[x\]/{s/\[x\]/\[ \]/}' ~/.$tdfile # on the first line containing [x] replace it with [ ]
                 else
-                    re='^[0-9]+$'
+                    re='^[0-9]+$' # regex to check if the argument is a number
 
-                    while [ $# -gt 0 ]; do
+                    while [ $# -gt 0 ]; do # while there are arguments
                         taskId="$1"
 
                         [[ ! $taskId =~ $re ]] && printf "${red}[ x ] : Invalid task id\n" && exit 1;
                         
                         printf "${blue}[ i ] : Marking task $taskId as undone\n"
-                        sed -i "${taskId}s/\[x\]/\[ \]/g" ~/.$tdfile
+                        sed -i "${taskId}s/\[x\]/\[ \]/g" ~/.$tdfile # on the specified line replace [x] with [ ]
                         shift
                     done
                 fi  
-                printTodo ~/.$tdfile
+                printTodoList ~/.$tdfile
             fi
         ;;
 
-        ua|undoall)
+        ua|undoall) # undo all tasks on the current todo list
             checkExist
-            [ $# -gt 1 ] && printf "${yellow}[ /i\\ ] : undoall doesn't require any arguments \n"
+            [ $# -gt 1 ] && printf "${yellow}[ /i\\ ] : undoall doesn't require any arguments \n" # if there is more than one argument print a warning because the arguments are ignored
 
 
             content=$(cat ~/.$tdfile)
@@ -202,15 +203,16 @@ function parseArgs() {
                 exit 1;
             else
                 printf "${blue}[ i ] : Marking all tasks as undone\n"
-                sed -i "s/\[x\]/\[ \]/g" ~/.$tdfile
-                printTodo ~/.$tdfile
+                sed -i "s/\[x\]/\[ \]/g" ~/.$tdfile # replace all [x] with [ ]
+                printTodoList ~/.$tdfile
             fi
         ;;
 
-        r|remove)
+        r|remove) # remove a task from the todo list
+
             checkExist
             shift  
-            taskId="${1:-last}"    
+            taskId="${1:-last}" # the task id to remove "last" if no argument is specified
 
             content=$(cat ~/.$tdfile)
             if [[ -z ${content// } ]]; then
@@ -220,54 +222,54 @@ function parseArgs() {
 
                 if [[ $taskId == 'last' ]]; then
                     printf "\r${blue}[ i ] : Removing the last task\n"
-                    sed -i "$(cat ~/.$tdfile|wc -l)d" ~/.$tdfile
+                    sed -i "$(cat ~/.$tdfile|wc -l)d" ~/.$tdfile # remove the last line
                 else
-                    re='^[0-9]+$'
+                    re='^[0-9]+$' # regex to check if the argument is a number
 
-                    taskId="$*"
+                    taskId="$*" # can only specify on task to remove because else we would have an error 
 
-                    [[ ! $taskId =~ $re ]] && printf "${red}[ x ] : Invalid task id, note that you can only specify one task : ${red}$taskId\n" && exit 1;
+                    [[ ! $taskId =~ $re ]] && printf "${red}[ x ] : Invalid task id, note that you can only specify one task : ${red}$taskId\n" && exit 1; # if the argument is not a number print error and exit
                     
                     printf "${blue}[ i ] : Removing task $taskId\n"
-                    sed -i "${taskId}d" ~/.$tdfile
+                    sed -i "${taskId}d" ~/.$tdfile # remove the specified line
                     shift
                 fi  
-                printTodo ~/.$tdfile
+                printTodoList ~/.$tdfile
 
             fi
 
         ;;
 
 
-        e|edit)
+        e|edit) # open the todo list in an editor
             checkExist
             shift
             editor=${1:-vi}
             $editor ~/.$tdfile
-            printTodo ~/.$tdfile
+            printTodoList ~/.$tdfile
         ;;
 
-        default|pa|printall)
-            lists_arr=($(ls ~/.dtodo*))
+        default|pa|printall) # print all the todo lists
+            lists_arr=($(ls ~/.dtodo*)) # list of all the existing todo lists
 
           
-            for list in "${lists_arr[@]}"
+            for list in "${lists_arr[@]}" # for each list
             do
                 printf "\n${purple}[ i ] : Printing list $(basename $list)\n"
-                printTodo $list
+                printTodoList $list # print it
             done
             
         ;;
 
-        uaa|undoallall)
+        uaa|undoallall) # undo all tasks on all todo lists
 
-            lists_arr=($(ls ~/.dtodo*))
+            lists_arr=($(ls ~/.dtodo*)) # the list of all the existing todo lists
 
-            for list in "${lists_arr[@]}"
+            for list in "${lists_arr[@]}" # for each list
             do
                 printf "\n${purple}[ i ] : Undoing all tasks in list $(basename $list)\n"
-                sed -i "s/\[x\]/\[ \]/g" $list
-                printTodo $list
+                sed -i "s/\[x\]/\[ \]/g" $list # replace all [x] with [ ]
+                printTodoList $list
             done
 
 
@@ -308,4 +310,4 @@ function parseArgs() {
 
 }
 
-parseArgs $@
+parseArgs $@ # Call the fonction with the arguments passed to the script
